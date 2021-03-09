6a48eae3070c43546f5913592f5923204fb9ff6e
Oliver Burt <oliburt@gmail.com>
2021-03-09 19:33:00 +0000

    gracefully cleanup threadpool by implementing Drop

--- src/lib.rs
+++ src/lib.rs
@@ -43,18 +43,29 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move || loop {
+        let thread = Some(thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
             println!("Worker {} got a job; executing.", id);
             job();
-        });
+        }));
         Worker { id, thread }
     }
 }
